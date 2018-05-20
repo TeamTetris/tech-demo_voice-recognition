11255a42c711960db8a10544387243e202b81a35
Maximilian Söchting <maximilian.soechting@student.hpi.de>
2018-05-20 12:06:21 +0200

    Add amplitude demo - technically works, but canvas does not clear & axes are confusing.

---
+++ amplitude_analyser/scripts/app.js
@@ -0,0 +1,243 @@
+// Older browsers might not implement mediaDevices at all, so we set an empty object first
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
+}
+
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+if (navigator.mediaDevices.getUserMedia === undefined) {
+  navigator.mediaDevices.getUserMedia = function(constraints) {
+
+    // First get ahold of the legacy getUserMedia, if present
+    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    // Some browsers just don't implement it - return a rejected promise with an error
+    // to keep a consistent interface
+    if (!getUserMedia) {
+      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+    }
+
+    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+    return new Promise(function(resolve, reject) {
+      getUserMedia.call(navigator, constraints, resolve, reject);
+    });
+  }
+}
+
+
+
+// set up forked web audio context, for multiple browsers
+// window. is needed otherwise Safari explodes
+
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var source;
+var stream;
+
+//set up the different audio nodes we will use for the app
+
+var analyser = audioCtx.createAnalyser();
+analyser.minDecibels = -60;
+analyser.maxDecibels = -10;
+analyser.smoothingTimeConstant = 0.85;
+
+var gainNode = audioCtx.createGain();
+
+var soundSource, concertHallBuffer;
+
+// set up canvas context for visualizer
+
+var canvas = document.querySelector('.visualizer');
+var canvasCtx = canvas.getContext("2d");
+
+var intendedWidth = document.querySelector('.wrapper').clientWidth;
+
+canvas.setAttribute('width',intendedWidth);
+
+var visualSelect = document.getElementById("visual");
+
+var drawVisual;
+
+//main block for doing the audio recording
+
+if (navigator.mediaDevices.getUserMedia) {
+   console.log('getUserMedia supported.');
+   var constraints = {audio: true}
+   navigator.mediaDevices.getUserMedia (constraints)
+      .then(
+        function(stream) {
+           source = audioCtx.createMediaStreamSource(stream);
+           source.connect(analyser);
+           analyser.connect(gainNode);
+           gainNode.connect(audioCtx.destination);
+
+        	 visualize();
+      })
+      .catch( function(err) { console.log('The following gUM error occured: ' + err);})
+} else {
+   console.log('getUserMedia not supported on your browser!');
+}
+
+function visualize() {
+  WIDTH = canvas.width;
+  HEIGHT = canvas.height;
+  var outputDiv = document.querySelector('.output');
+  var maxDecibelsQueue = new Queue();
+
+
+  var visualSetting = visualSelect.value;
+  console.log(visualSetting);
+
+  if(visualSetting == "sinewave") {
+    analyser.fftSize = 2048;
+    var bufferLength = analyser.fftSize;
+    console.log(bufferLength);
+    var dataArray = new Uint8Array(bufferLength);
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    var draw = function() {
+
+      drawVisual = requestAnimationFrame(draw);
+
+      analyser.getByteTimeDomainData(dataArray);
+
+      canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+      canvasCtx.beginPath();
+
+      var sliceWidth = WIDTH * 1.0 / bufferLength;
+      var x = 0;
+
+      for(var i = 0; i < bufferLength; i++) {
+
+        var v = dataArray[i] / 128.0;
+        var y = v * HEIGHT/2;
+
+        if(i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(canvas.width, canvas.height/2);
+      canvasCtx.stroke();
+    };
+
+    draw();
+
+  } else if(visualSetting == "frequencybars") {
+    analyser.fftSize = 256;
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+    var dataArray = new Uint8Array(bufferLength);
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    var drawAlt = function() {
+      drawVisual = requestAnimationFrame(drawAlt);
+
+      analyser.getByteFrequencyData(dataArray);
+
+      canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      var barWidth = (WIDTH / bufferLength) * 2.5;
+      var barHeight;
+      var x = 0;
+
+      for(var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+
+        canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+        canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
+
+        x += barWidth + 1;
+      }
+    };
+
+    drawAlt();
+
+  } else if(visualSetting == "amplitude") {
+    analyser.fftSize = 2048;
+    var bufferLength = analyser.fftSize;
+    var dataArray = new Uint8Array(bufferLength);
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    var draw = function() {
+      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+      drawVisual = requestAnimationFrame(draw);
+
+      analyser.getByteTimeDomainData(dataArray);
+
+      function calculateDecibels (sample) {
+        var amplitude = Math.abs(sample - 128) / 128.0;
+        var decibels = 20 * Math.log10(amplitude);
+        return decibels;
+      }
+
+      var maxDecibels = -99;
+      for (var i = 0; i < dataArray.length; i++) {
+        var decibels = calculateDecibels(dataArray[i])
+        if (decibels > maxDecibels) {
+          maxDecibels = decibels;
+        }
+      }
+      outputDiv.innerHTML = Math.round(maxDecibels, 2) + " dB";
+
+      maxDecibelsQueue.add(maxDecibels);
+      if (maxDecibelsQueue.size() > 100) {
+        maxDecibelsQueue.remove();
+      } else {
+        return;
+      }
+
+      var sliceWidth = WIDTH * 1.0 / maxDecibelsQueue.size();
+      var x = 0;
+
+      for(var i = 0; i < maxDecibelsQueue.size(); i++) {
+
+        var v = (maxDecibelsQueue.data[i] + 43) / 43.0;
+        var y = v * HEIGHT;
+
+        if(i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(canvas.width, canvas.height);
+      canvasCtx.stroke();
+    };
+
+    draw();
+  } else if(visualSetting == "off") {
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+    canvasCtx.fillStyle = "red";
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+  }
+
+}
+
+
+
+// event listeners to change visualize and voice settings
+
+visualSelect.onchange = function() {
+  window.cancelAnimationFrame(drawVisual);
+  visualize();
+};
+
